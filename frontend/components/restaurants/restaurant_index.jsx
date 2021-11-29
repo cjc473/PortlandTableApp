d88a2b09a56e9579940af7064e0eea014b42dfa6
cjc473 <chris.cheasty3@gmail.com>
2021-11-28 22:36:48 -0800

    initial styling for homepage restaurants

--- frontend/components/restaurants/restaurant_index.jsx
+++ frontend/components/restaurants/restaurant_index.jsx
@@ -13,18 +13,21 @@ class RestaurantIndex extends React.Component {
   render() {
     const {restaurants} = this.props
     return (
-      <div>
-        <h2 className="homepage-restaurant-header">Book for dinner tonight</h2>
-        <ul className="homepage-restaurant-list">
-          {(restaurants).map(restaurant => (
-            <RestaurantIndexItem restaurant={restaurant} key={restaurant.id} />
-          ))}
-        </ul>
-        <h2 className="homepage-restaurant-header">Outdoor dining</h2>
-
-        <h2 className="homepage-restaurant-header">New to PortlandTable</h2>
-
-
+      <div className="homepage-restaurants-container">
+        <div className="homepage-restaurant-header-container">
+          <h2 className="homepage-restaurant-header">Book for dinner tonight</h2>
+          <ul className="homepage-restaurant-list">
+            {(restaurants).map(restaurant => (
+              <RestaurantIndexItem restaurant={restaurant} key={restaurant.id} />
+            ))}
+          </ul>
+        </div>
+        <div className="homepage-restaurant-header-container">
+          <h2 className="homepage-restaurant-header">Outdoor dining</h2>
+        </div>
+        <div className="homepage-restaurant-header-container">
+          <h2 className="homepage-restaurant-header">New to PortlandTable</h2>
+        </div>
       </div>
     )
   }
