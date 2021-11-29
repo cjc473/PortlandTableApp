1b249b923342af4b637a38240f0131d39ef3093f
cjc473 <chris.cheasty3@gmail.com>
2021-11-29 09:26:37 -0800

    Restaurant show page

---
+++ frontend/components/restaurants/restaurant_show.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class RestaurantShow extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>this is a show page!</h1>
+      </div>
+    )
+  }
+}
+
+export default RestaurantShow
