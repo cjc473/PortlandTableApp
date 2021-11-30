e2e239fd4e70a86b9acd7806b5048bc066eba802
cjc473 <chris.cheasty3@gmail.com>
2021-11-29 19:11:21 -0800

    add homepage link to logo

--- frontend/components/navbar/logo.jsx
+++ frontend/components/navbar/logo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <div className="logo">
-      <img src="https://portlandtable.s3.us-west-2.amazonaws.com/banner+images/pngaaa.com-443257.png" alt="image"/>
-      <h2>PortlandTable</h2>
-    </div>
+      <div className="logo">
+        <Link to='/'><img src="https://portlandtable.s3.us-west-2.amazonaws.com/banner+images/pngaaa.com-443257.png" alt="image" /></Link>
+        <Link to='/'><h2>PortlandTable</h2></Link>
+      </div>
   )
 }
 
