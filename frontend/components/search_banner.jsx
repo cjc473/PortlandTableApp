8d62fabd414670d8c4de12ee0dfe10bd8bf611bf
cjc473 <chris.cheasty3@gmail.com>
2021-11-30 00:24:30 -0800

    complete initial styling of search banner

--- frontend/components/search_banner.jsx
+++ frontend/components/search_banner.jsx
@@ -2,15 +2,55 @@ import React from "react";
 
 const SearchBanner = () => {
 
+  const todaysDate = new Date();
+  const year = todaysDate.getFullYear();
+  const day = todaysDate.getDate();
+  const month = todaysDate.getMonth() + 1;
+
   return (
     <div className="banner-container">
       <h2 className="banner-message">Find your table for any occasion</h2>
-      <div className="search-bar-container">
-        <div>
-          
+      <div className="search-bar-container"> 
+        <div className="table-options">
+          <input type="date" 
+            className="date-select" 
+            min={`${year}-${month}-${day}`}
+            defaultValue={`${year}-${month}-${day}`}
+          />
+          <select type="text" className="time-select">
+            <option value="1100am">11:00 AM</option>
+            <option value="1130am">11:30 AM</option>
+            <option value="1200pm">12:00 PM</option>
+            <option value="1230pm">12:30 PM</option>
+            <option value="1000am">6:00 PM</option>
+            <option value="1030am">6:30 PM</option>
+            <option value="1100am">7:00 PM</option>
+            <option value="1130am">7:30 PM</option>
+            <option value="1200pm">8:00 PM</option>
+            <option value="1230pm">8:30 PM</option>
+          </select>
+          <select type="text" className="size-select">
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
         </div>
-        <div>
-          
+        <div className="table-options-right">
+          <input type="text"
+            value=""
+            className="search-field"
+            placeholder="Restaurant or Cuisine"
+          />
+          <input type="submit"
+            value="Let's go"
+            className="search-submit"
+          />
         </div>
       </div>
     </div>
