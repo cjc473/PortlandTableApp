87a34089ed62b2a68831ceebfff3a191c8041e4d
cjc473 <chris.cheasty3@gmail.com>
2021-11-30 23:37:24 -0800

    add initial components for reservation

---
+++ frontend/components/reservations/edit_reservation_container.js
@@ -0,0 +1,16 @@
+// import { connect } from "react-redux";
+// import ReservationForm from "./reservation_form";
+// import { createReservation } from "../../actions/reservation_actions";
+
+
+// const mSTP = state => ({
+//   user_id: state.session.id,
+//   formType: "Edit Reservation"
+// })
+
+// const mDTP = dispatch => ({
+//   action: reservation => dispatch(createReservation(reservation))
+// })
+
+
+export default connect(mSTP)(mDTP)(ReservationForm)
---
+++ frontend/components/reservations/reservation_form.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ReservationForm extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <h1>this is a reservation form</h1>
+    )
+  }
+}
+
+export default ReservationForm
