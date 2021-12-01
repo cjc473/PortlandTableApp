78415ff079b8e899f24a91450bd2d3ac92031690
cjc473 <chris.cheasty3@gmail.com>
2021-11-30 20:21:03 -0800

    add reservation backend routes

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -10,21 +10,35 @@ class Api::ReservationsController < ApplicationController
   end
 
   def create
-
-  end
-
-  def destroy
-
+    @reservation = Reservation.create(reservation_params)
+    if @reservation.save
+      render :show
+    else
+      render json: @reservation.errors.full_messages, status: 422
+    end
   end
 
   def update
+    @reservation = Reservation.find(params[:id])
+    if @reservation.update(reservation_params)
+      render :show
+    else
+      render json: @reservation.errors.full_messages, status: 422
+    end
+  end
 
+  def destroy
+    @reservation = Reservation.find(params[:id])
+    if @reservation.destroy
+    else 
+      render json: ['Unable to delete reservation, must be logged in as correct user'], status: 422
+    end
   end
 
   private
 
   def reservation_params
-    params.require(:reservation).permit
+    params.require(:reservation).permit(:restaurant_id, :user_id, :party_size, :date, :time)
   end
 
 end
---
+++ app/views/api/reservations/_reservation.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! reservation, :id, :party_size, :restaurant_id, :user_id, :date, :time
