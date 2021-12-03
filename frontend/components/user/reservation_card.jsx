0dcd6b023eea6ca01d54ad7097d86af04747aced
cjc473 <chris.cheasty3@gmail.com>
2021-12-02 17:26:54 -0800

    restaurants show up in reservation card

--- frontend/components/user/reservation_card.jsx
+++ frontend/components/user/reservation_card.jsx
@@ -3,22 +3,26 @@ import React from "react";
 class ReservationCard extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      
-    }
+    // this.state = {
+    //   reservation: this.props.reservation,
+    //   restaurant: this.props.restaurants[reservation.restaurant_id]
+    // }
   }
 
   componentDidMount() {
-    this.props.fetchRestaurants()
+    // this.props.fetchRestaurants()
   }
 
+
+
   render() {
-    if (!this.props.restaurants) return null;
-    const {reservation} = this.props;
+    if ((!this.props.restaurant) || (!this.props.reservation)) return null;
+    const {reservation, restaurant} = this.props;
     console.log("props below")
-    console.log(this.props)
-    const restaurant = this.props.restaurants[reservation.restaurant_id]
+    // console.log(this.props)
+    // const restaurant = this.props.restaurants[reservation.restaurant_id]
     console.log(restaurant)
+    // console.log(restaurant.id)
     return (
       <div>
         <p>{restaurant.name}</p>
--- frontend/components/user/reservation_card_container.jsx
+++ frontend/components/user/reservation_card_container.jsx
@@ -3,12 +3,18 @@ import { connect } from "react-redux";
 import ReservationCard from "./reservation_card";
 import { fetchRestaurants } from "../../actions/restaurant_actions";
 
-const mSTP = state => ({
-  restaurants: state.entities.restaurants
-})
+const mSTP = (state, ownProps) => {
+  // debugger;
+  // restaurants: state.entities.restaurants,
+  return ({
+    restaurant: state.entities.restaurants[ownProps.reservation.restaurant_id]
+  })
+}
 
-const mDTP = dispatch => ({
-  fetchRestaurants: () => dispatch(fetchRestaurants())
-})
 
-export default connect(mSTP, mDTP)(ReservationCard)
+
+// const mDTP = dispatch => ({
+//   fetchRestaurants: () => dispatch(fetchRestaurants())
+// })
+
+export default connect(mSTP, null)(ReservationCard)
