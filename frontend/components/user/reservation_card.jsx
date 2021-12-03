fcc50ca40a44191eedc6db59cc70db51c0c60507
cjc473 <chris.cheasty3@gmail.com>
2021-12-03 00:08:50 -0800

    Additional res card styling

--- frontend/components/user/reservation_card.jsx
+++ frontend/components/user/reservation_card.jsx
@@ -28,12 +28,19 @@ class ReservationCard extends React.Component {
     return (
       <div className="reservation-card-container">
         <div className="reservation-card-pic"><img src={restaurant.photo} alt="" /></div>
-        <div className="reservation-card-details">
-          <h2>{restaurant.name}</h2>
-          <p><BiTimeFive font-size="24px" /> &nbsp;&nbsp;{reservation.time}</p>
-          <p><BiCalendarAlt font-size="24px" /> &nbsp;&nbsp;{reservation.date}</p>
-          <p><BiUserCircle font-size="24px" /> &nbsp;&nbsp;Table for {reservation.party_size}</p>
+        <div className="reservation-card-text-container">
+          <div className="reservation-card-details">
+            <h2>{restaurant.name}</h2>
+            <p><BiTimeFive font-size="24px" /> &nbsp;&nbsp;{reservation.time}</p>
+            <p><BiCalendarAlt font-size="24px" /> &nbsp;&nbsp;{reservation.date}</p>
+            <p><BiUserCircle font-size="24px" /> &nbsp;&nbsp;Table for {reservation.party_size}</p>
+          </div>
+          <div className="reservation-card-button-container">
+            <div className="reservation-card-button">Modify</div>
+            <div className="reservation-card-button">Cancel</div>
+          </div>
         </div>
+
       </div>
     )
   }
