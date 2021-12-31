93d4931ad9234f9f39895ec702ec73f8d28bed1c
cjc473 <chris.cheasty3@gmail.com>
2021-12-31 11:59:06 -0800

    create review form component

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+
+class ReviewForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: "",
+      score: null,
+      restaurant_id: this.props.restaurantId,
+      author_id: this.props.userId
+    }
+  }
+}
