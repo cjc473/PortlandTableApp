5bf9e95df26984eb4153960d967595c92adaa8a6
cjc473 <chris.cheasty3@gmail.com>
2021-12-31 18:31:13 -0800

    restaurant id correctly threading

--- frontend/components/reviews/review_form_container.js
+++ frontend/components/reviews/review_form_container.js
@@ -2,11 +2,11 @@ import { connect } from "react-redux";
 import ReviewForm from "./review_form";
 
 const mSTP = (state, ownProps) => ({
-
+  restaurantId
 })
 
 const mDTP = (dispatch, ownProps) => ({
-  
+
 })
 
 export default connect(mSTP, mDTP)(ReviewForm)
