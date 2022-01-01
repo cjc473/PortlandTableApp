46d191234bdf4bf905933ff0a2a0afd9aa9f96e7
cjc473 <chris.cheasty3@gmail.com>
2021-12-31 16:45:16 -0800

    debug form

--- frontend/components/reviews/review_form_container.js
+++ frontend/components/reviews/review_form_container.js
@@ -1,4 +1,12 @@
 import { connect } from "react-redux";
 import ReviewForm from "./review_form";
 
+const mSTP = (state, ownProps) => ({
+
+})
+
+const mDTP = (dispatch, ownProps) => ({
+  
+})
+
 export default connect(mSTP, mDTP)(ReviewForm)
