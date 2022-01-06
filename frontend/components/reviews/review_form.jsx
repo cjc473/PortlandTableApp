8e5498da38657c63ad951d7b4d3e6e45fc0ccd5e
cjc473 <chris.cheasty3@gmail.com>
2022-01-05 18:44:55 -0800

    add inputs to review form

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -11,16 +11,44 @@ class ReviewForm extends React.Component {
       author_id: this.props.userId
     }
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.update = this.update.bind(this)
   }
 
   handleSubmit(e) {
     e.preventDefault();
   }
 
+  update(field) {
+    return e => (
+      this.setState({ [field]: e.target.value })
+    )
+  }
+
   render() {
     return (
       <div>
-        <form ></form>
+        <form>
+          <h2>Write your review</h2>
+          <input type="text" 
+            className="review-title"
+            value={this.state.title}
+            onChange={this.update("title")}
+            placeholder="Title goes here"
+            />
+          <textarea name="" 
+            cols="30" 
+            rows="10"
+            className="review-body"
+            value={this.state.body}
+            placeholder="Body goes here" 
+            />
+          <input type="text"
+            
+            value={this.state.title}
+            onChange={this.update("title")}
+            placeholder="Title"
+          />
+        </form>
       </div>
     )
   }
