8b4bb526c3b6bfee75e466d1d0d02e164cfea76e
cjc473 <chris.cheasty3@gmail.com>
2022-01-10 16:40:55 -0800

    add reservation date comparison

--- frontend/components/user/profile.jsx
+++ frontend/components/user/profile.jsx
@@ -13,17 +13,38 @@ class Profile extends React.Component {
     this.props.fetchRestaurants();
   }
 
+  isPastDate(date) {
+    const todaysDate = new Date();
+    if (date.getFullYear() > todaysDate.getFullYear()) return false;
+    if (date.getFullMonth() > todaysDate.getMonth()) return false;
+    if (date.getDate() > todaysDate.getDate()) return false;
+    return true;
+  }
+
+  sortReservations(filter, reservations) {
+    const past_res = reservations.filter(res => this.isPastDate(res))
+    const future_res = reservations.filter(res => !(this.isPastDate(res)))
+    return filter === past ? past_res : future_res
+  }
+
   render() {
     if (!this.props.currentUser) return null;
     const { reservations } = this.props;
-    // if (!reservations) return null;
     return(
       <div className="profile-page-container">
         <div className="user-welcome">
           <h2 id="welcome-msg">Hi, {this.props.currentUser.first_name}.</h2>
         </div>
-        <div className="user-reservations-container">
+        <div id="upcoming-res-container" className="user-reservations-container">
           <h2 className="profile-list-header">Upcoming Reservations</h2>
+          <div className="user-reservations-list">
+            <ul>
+              {reservations ? reservations.map(res => <ReservationCardContainer reservation={res} />) : "No upcoming res"}
+            </ul>
+          </div>
+        </div>
+        <div id="past-res-container" className="user-reservations-container">
+          <h2 className="profile-list-header">Dining History</h2>
           <div className="user-reservations-list">
             <ul>
               {reservations ? reservations.map(res => <ReservationCardContainer reservation={res} />) : "No upcoming res"}
